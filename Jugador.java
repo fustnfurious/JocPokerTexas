1d1d34d14874c92005e382ba137366126d3ba57f
Isaac Huguet <36538409+huguet12@users.noreply.github.com>
2020-11-18 17:25:47 +0100

    update a jugador

--- Jugador.java
+++ Jugador.java
@@ -1,16 +1,58 @@
 
 public class Jugador {
 	
-	protected Carta carta1;
-	protected Carta carta2;
+	protected Ma ma;
 	protected int diners;
+	protected int diners_apostats_enjoc;
+	protected boolean primerTorn;
+	protected String nom;
+	protected boolean is_playing;
 	
-	public Object getMa() {
-		return new Object() {
-			Carta c1 = carta1;
-			Carta c2 = carta2;
-		};
+
+	public Jugador(String nom) {
+		this.nom = nom;
+		this.primerTorn = false;
 	}
 	
+	public Jugador(String nom, int diners) {
+		this.nom = nom;
+		this.primerTorn = false;
+		this.diners = diners;
+		this.diners_apostats_enjoc = 0;
+	}
+	
+	public void nova_Ma(Baralla baralla) {
+		this.ma = new Ma(baralla);
+	}
+	
+	public void getMa() {
+		ma.printMa();
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public int apostar(int aposta) {
+		this.diners -= aposta;
+		return this.diners;
+	}
+	
+	public void fold() {
+		this.is_playing = false;
+	}
+
+	public int getDiners() {
+		return this.diners;
+	}
+	
+	public void updateDiners_apostats(int diners) {
+		this.diners_apostats_enjoc = diners;
+	}
+
+	public int getDinersApostats() {
+		
+		return this.diners_apostats_enjoc;
+	}
 	
 }
