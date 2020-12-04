6afdd9baed17b4b0b799f7ba23b1c4397a417009
Isaac Huguet <36538409+huguet12@users.noreply.github.com>
2020-12-04 15:02:01 +0100

    mhavia deixat aquest eheh

--- Partida.java
+++ Partida.java
@@ -1,55 +1,82 @@
+package PokerModel;
+import java.net.ServerSocket;
 import java.util.*;
+import java.util.concurrent.*;
 
-public class Partida {
+
+public class Partida extends Thread{
 	
-	protected ArrayList<Jugador> jugadors;
-	protected Joc joc_actiu;
-	private boolean nou_primer_torn = true;
+	public ArrayList<ClientThread> jugadors;
+	private Joc joc_actiu;
+	private boolean nou_primer_torn = true;	
+	boolean exit_partida = true;
+	private Servidor ss;
+	int index_torn_absolut;
+	int f = 0;
 	
 	
 	public Partida() {
 		this.jugadors = new ArrayList<>();
 	}
 	
+	public Partida(Servidor ss) {
+		this.jugadors = new ArrayList<>();
+		this.ss = ss;
+	}
+		
 	private int canviar_primer_torn() {
 //		System.out.println(jugadors_actius.size());
 		for(int i=0 ; i < jugadors.size() ; i++) {
 //			System.out.println(i);
-			if(jugadors.get(i).primerTorn == true) {
-				jugadors.get(i).primerTorn = false;
-				jugadors.get((i+1)%jugadors.size()).primerTorn = true;
+			if(jugadors.get(i).jugador.primerTorn == true) {
+				jugadors.get(i).jugador.primerTorn = false;
+				jugadors.get((i+1)%jugadors.size()).jugador.primerTorn = true;
 				return i+1;
 			}
 		}
 		if(nou_primer_torn == true) {
-			jugadors.get(0).primerTorn = true;
+			jugadors.get(0).jugador.primerTorn = true;
 			nou_primer_torn = false;
 			return 0;
 		}
 		return -1;
 	}
 	
-	public static void main(String[] args) {
-		Partida partida = new Partida();
-		int f = 0;
-		boolean exit_partida = true;
-		int index_torn_absolut;
-		partida.jugadors.add(new Jugador("lluis", 10));
-		partida.jugadors.add(new Jugador("isaac", 12));
-		partida.jugadors.add(new Jugador("oriol", 14));
-		
+	public void run() {
 		while(exit_partida) {
-			index_torn_absolut = partida.canviar_primer_torn();
-			partida.joc_actiu = new Joc(partida.jugadors);
-			partida.joc_actiu.generar_joc(index_torn_absolut);
-			if(f == 2z) {
-				exit_partida = false;
-			}else {
-				f++;
-			}
+			if(jugadors.size() >= 1) {
+				index_torn_absolut = canviar_primer_torn();
+				joc_actiu = new Joc(jugadors);
+				joc_actiu.generar_joc(index_torn_absolut);
+				try {
+					Thread.sleep(15000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			
+			} else {
+				System.out.println("esperant jugadors...");
+				try {
+					Thread.sleep(30000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-
 	}
 	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		boolean exit_partida = true;
+		int index_torn_absolut;
+		int f = 0;
+		Partida partida = new Partida();
+//		partida.jugadors.add(new Jugador("lluis", 10));
+//		partida.jugadors.add(new Jugador("isaac", 12));
+//		partida.jugadors.add(new Jugador("oriol", 14));
+//		partida.jugadors.add(new Jugador("helena", 14));
+//		partida.jugadors.add(new Jugador("julia", 14));
+//		partida.jugadors.add(new Jugador("sandra", 14));
+//		PlayerInterface p1 = new PlayerInterface(partida.jugadors.get(0));
+		
+	}
 }
